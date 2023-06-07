fdc05271c6c8455859f967e1604ee02d533ab914
Rusira Pathum <48381430+RusiraPathum@users.noreply.github.com>
2023-06-07 16:14:41 +0530

    done

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -15,12 +15,12 @@ $("#submit").click(function (){
     let subject = document.getElementById('subject').value;
     let message = document.getElementById('message').value;
 
-    // var status = validateItemForm();
-    // if (status != true) {
-    //     $("#alertError").text(status);
-    //     $("#alertError").show();
-    //     return;
-    // }
+    var status = validateItemForm();
+    if (status != true) {
+        $("#alertError").text(status);
+        $("#alertError").show();
+        return;
+    }
 
     let id = "contact";
 
@@ -36,35 +36,35 @@ $("#submit").click(function (){
         url: './data.php',
         type: 'POST',
         data: dt,
-        // beforeSend: function(){
-        //     // $("#loading").show();
-        //     $("#submit").attr("disabled", "disabled");
-        // },
+        beforeSend: function(){
+            // $("#loading").show();
+            $("#submit").attr("disabled", "disabled");
+        },
         success:function (res){
-            console.log(res);
-            // if (res == 1){
-            //     $("#alertError").hide();
-            //     $("#alertSuccess").text("Your email sent Successfully, Thank you.");
-            //     $("#alertSuccess").fadeTo(2000, 500).slideUp(500, function() {
-            //         $("#alertSuccess").slideUp(500);
-            //     });
+            // console.log(res);
+            if (res == 1){
+                $("#alertError").hide();
+                $("#alertSuccess").text("Your email sent Successfully, Thank you.");
+                $("#alertSuccess").fadeTo(2000, 500).slideUp(500, function() {
+                    $("#alertSuccess").slideUp(500);
+                });
 
-            //     // $("#loading").hide();
+                // $("#loading").hide();
 
-            //     document.getElementById('name').value = "";
-            //     document.getElementById('email').value = "";
-            //     document.getElementById('subject').value = "";
-            //     document.getElementById('message').value = "";
+                document.getElementById('name').value = "";
+                document.getElementById('email').value = "";
+                document.getElementById('subject').value = "";
+                document.getElementById('message').value = "";
 
-            //     $("#submit").prop("disabled", false);
+                $("#submit").prop("disabled", false);
 
-            // }else {
-            //     $("#alertError").text("Error occurred while sending email. Please try again later.");
-            //     $("#alertError").fadeTo(2000, 500).slideUp(500, function() {
-            //         $("#alertError").slideUp(500);
-            //     });
-            //     $("#submit").prop("disabled", false);
-            // }
+            }else {
+                $("#alertError").text("Error occurred while sending email. Please try again later.");
+                $("#alertError").fadeTo(2000, 500).slideUp(500, function() {
+                    $("#alertError").slideUp(500);
+                });
+                $("#submit").prop("disabled", false);
+            }
         },
         error: function (request, error) {
             console.log("Request " +request +"----" + error);
